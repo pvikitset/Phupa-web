de2af251c95ce0a3d512687bd7f974d9c621b46b
Paul Vikitset <p.vikitset@hotmail.com>
2019-01-09 14:38:33 -0600

    contact page

--- js/google-map.js
+++ js/google-map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(12.919906, 102.270786); //12.919906, 102.270786
     // 39.399872
     // -8.224454
     
